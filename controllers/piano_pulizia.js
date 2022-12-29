d100eaa77479fb53c14335800773c7db6bfa9852
CloudRider <smlxmpr01@gmail.com>
2022-12-29 15:30:36 +0100

    fixed error in cleaning plan API

--- controllers/piano_pulizia.js
+++ controllers/piano_pulizia.js
@@ -7,7 +7,7 @@ const createPianoPulizia = (req, res) => {
     let user = req.loggedUser;
 
     const { id_zona, data_inizio, data_fine, nome_organizzazione } = req.body;
-    if (!zona || !data_inizio || !data_fine || !nome_organizzazione) {
+    if (!id_zona || !data_inizio || !data_fine || !nome_organizzazione) {
         return res.status(400).json({ Error: "Bad request: missing parameters" });
     }
 
